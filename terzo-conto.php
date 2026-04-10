787373ec50b5b56ab49cc842324045dd53dcb28a
mjfan80 <mjfan80@gmail.com>
2026-04-10 21:47:52 +0200

    Update tested version to 6.9

--- terzo-conto.php
+++ terzo-conto.php
@@ -5,7 +5,7 @@
  * Version: 1.0.0
  * Requires at least: 6.0
  * Requires PHP: 7.4
- * Tested up to: 6.8
+ * Tested up to: 6.9
  * Author: TerzoConto Contributors
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
